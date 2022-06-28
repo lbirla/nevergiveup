cfed5b1ef6d80544383af2606e4d7c5e0369e4e8
lalit <lbirla64@gmail.com>
2022-06-28 18:40:19 +0530

    dlj

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def index():
     mssg = "starting of testing"
     return render_template('result.html',mssg=mssg)
 
-port = int(os.getenv("PORT", 500))
+port = int(os.getenv("PORT", 200))
 
 if __name__ == "__main__":
     host = '0.0.0.0'
