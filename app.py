b6938bab2cf97e434c9ef1a015149d786d2c5dc4
lalit <lbirla64@gmail.com>
2022-06-28 15:23:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template,request
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    mssg="starting of testing"
+    return render_template('result.html',mssg=mssg)
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
