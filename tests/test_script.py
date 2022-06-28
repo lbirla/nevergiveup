a3e93ec598f82eb1fc8b192dfb809e6bb1fdebb0
lalit <lbirla64@gmail.com>
2022-06-28 18:03:06 +0530

    remove -v tests/test_script.pyy

--- tests/test_script.py
+++ tests/test_script.py
@@ -6,6 +6,7 @@ import os
 
 class TestToPerform(unittest.TestCase):
     def setUp(self):
+        print('appclient:   ',app.test_client())
         self.app = app.test_client()
 
     def tearDown(self):
@@ -13,8 +14,10 @@ class TestToPerform(unittest.TestCase):
 
     def test_page(self):
         response = self.app.get('/', follow_redirects=True)
-        print(response)
+        print('response:  ',response)
+        print('response status: ',response.status_code)
         self.assertEqual(response.status_code, 200)
+
 
 
 if __name__ == '__main__':
